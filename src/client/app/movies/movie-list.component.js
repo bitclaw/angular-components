c7e2769f908cfce47efd29c49973fd77c98186c9
Daniel Chavez <bitclaw@gmail.com>
2016-05-27 08:11:51 -0500

    Add controller as syntax.

--- src/client/app/movies/movie-list.component.js
+++ src/client/app/movies/movie-list.component.js
@@ -5,6 +5,7 @@
     .module('app.movies')
     .component('movieList', {
       templateUrl: 'app/movies/movie-list.component.html',
+      controllerAs: 'vm',
       controller: function () {
         this.message = 'Hello from a component controller';
       }
